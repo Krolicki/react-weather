6d3e658f390f472c4b04909ae01bc33f8196bb9f
Jakub Królicki <jakub.krolicki.k@gmail.com>
2022-10-29 22:55:37 +0200

    add input icons

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -1,5 +1,7 @@
 import { useState } from "react"
 import './Search.css'
+import {TbSearch} from 'react-icons/tb'
+import {GrClose} from 'react-icons/gr'
 
 export const Search = ({onSearch, setLoading}) => {
     const [query, setQuery] = useState("")
@@ -11,13 +13,27 @@ export const Search = ({onSearch, setLoading}) => {
     }
 
     return (
-        <form onSubmit={handleSubmit}>
-            <input 
-                placeholder="Wpisz miasto..."
-                value={query}
-                onChange={e=>{ setQuery(e.target.value)}}
-                className="search-input"
-            />
-        </form>
+        <div className="search-wraper">
+            <form onSubmit={handleSubmit} className="search-form">
+                <input 
+                    placeholder="Wpisz miasto..."
+                    value={query}
+                    onChange={e=>{ setQuery(e.target.value)}}
+                    className="search-input"
+                />
+                {query !== "" ?
+                <GrClose 
+                    size={26} 
+                    className="search-clear"
+                    onClick={()=>setQuery("")}
+                />
+                : <></>}
+                <span className="search-line" ></span>
+                <button type="submit">
+                    <TbSearch size={25} className="search-icon"
+                    />
+                </button>
+            </form>
+        </div>
     )
 }
